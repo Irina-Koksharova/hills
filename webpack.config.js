1acd0facb0c3e09a255586504099bb6ed1ca1307
Iryna Koksharova <iryna.koksharova@valtech.com>
2024-10-01 12:56:47 +0300

    #2: Add header, change webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,17 +8,24 @@ const devMode = process.env.NODE_ENV !== 'production';
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
     clean: true,
   },
   plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map[query]'
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
   ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
   module: {
     rules: [
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
       {
         test: /\.(sa|sc|c)ss$/i,
         use: [
@@ -28,16 +36,21 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[hash][ext]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name]-[hash][ext]',
+        },
       },
     ],
   },
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
